38f55a498735a3826e3da73ab7d7ad6e0895043e
dwivedi-ritik <dwi.ritik20@gmail.com>
2021-07-31 15:46:22 +0530

    removed loaded image

--- eventHandle/eventHandle.js
+++ eventHandle/eventHandle.js
@@ -6,13 +6,32 @@ const branch = document.querySelector(".branch")
 
 const eventForm = document.querySelector(".signup")
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCyGB8LMzEVHSCUlvCY3bzp8uFpLnWQUAQ",
+    authDomain: "anukunj-b5764.firebaseapp.com",
+    projectId: "anukunj-b5764",
+    storageBucket: "anukunj-b5764.appspot.com",
+    messagingSenderId: "290958606747",
+    appId: "1:290958606747:web:520f186fe019450577a13e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 eventForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    let usrObj = {
-        "firstName": firstName.value,
-        "lastname": lastName.value,
-        "email": email.value,
-        "mobile": mobileNum,
-    }
-    console.log(usrObj)
+    firebase.firestore().collection("events").add({
+        "first_name": firstName.value,
+        "last_name": lastName.value,
+        "mobile": 7267862,
+        "email": email.value
+    })
+        .then(res => {
+            alert("Docs added")
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    // console.log(usrObj)
 })
